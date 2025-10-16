9d5fb0beaa0fadf731376cdd8c4ddef365be7fc8
Bahri Bahri <aribahri15@gmail.com>
2025-10-17 01:32:38 +0700

    fix: detail course

--- resources/views/front/course.blade.php
+++ resources/views/front/course.blade.php
@@ -57,76 +57,20 @@
                         <div class="col-lg-4 col-md-6 d-flex align-items-stretch mb-3" data-aos="zoom-in"
                             data-aos-delay="100">
                             <div class="course-item">
-                                @guest
-
-                                    <a href="{{ route('front.course.details', $course->slug) }}">
-                                        <img src="{{ Storage::url($course->image ?? '') }}" class="" alt="..."
-                                            style="width: 100%; height: 274px; display: block;"></a>
-                                @endguest
-                                @auth
-
-
-                                    <a href="{{ route('course.curriculam', $course->id) }}">
-                                        <img src="{{ Storage::url($course->image ?? '') }}" class="" alt="..."
-                                            style="width: 100%; height: 274px; display: block;"></a>
-                                @endauth
-
+                                <a href="{{ route('front.course.category', $course->category) }}">
+                                    <img src="{{ Storage::url($course->image ?? '') }}" class="" alt="..."
+                                        style="width: 100%; height: 274px; display: block;"></a>
                                 <div class="course-content">
                                     <div class="d-flex justify-content-between align-items-center mb-3 flex-wrap gap-2">
-                                        @guest
-                                            <a href="{{ route('register.with.course') }}?course_id={{ $course->id }}"
-                                                class="btn btn-primary">Login to Enroll</a>
-
-                                            {{-- <a href="{{ route('register') }}" class="btn btn-primary">Login to Enroll</a> --}}
-                                        @else
-                                            @if (auth()->user()->hasApprovedCourse($course->id))
-                                                <button class="btn btn-success" disabled>Already Enrolled</button>
-                                            @else
-                                                <!-- If you want to show 'Up Coming' based on course status, you can do something like: -->
-                                                @if ($course->status === 'upcoming')
-                                                    <button class="btn btn-secondary" disabled>Up Coming</button>
-                                                @elseif(auth()->user()->hasPurchasedCourse($course->id))
-                                                    <button class="btn btn-info w-100" disabled>Request Pending...</button>
-                                                @else
-                                                    <!-- Instead of directly purchasing, go to the enroll form -->
-                                                    <a href="{{ route('stripe.payment.view', $course->id) }}"
-                                                        class="btn btn-primary">Enroll
-                                                        Now</a>
-                                                @endif
-                                            @endif
-                                        @endguest
-                                        @guest
-
-                                            <a href="{{ route('front.course.details', $course->slug) }}"
-                                                class="btn btn-info">More Details</a>
-                                        @endguest
-                                        @auth
-                                            <a href="{{ route('course.curriculam', $course->id) }}" class="btn btn-info">More
-                                                Details</a>
-                                        @endauth
-                                        <!--<p class="category">Enroll Now</p>-->
-                                        @guest
-
-                                            <p class="price">${{ $course->price ?? '' }}</p>
-                                        @endguest
+                                        <a href="{{ route('front.course.category', $course->category) }}"
+                                            class="btn btn-info">More Details</a>
+                                        <p class="price">${{ $course->price ?? '' }}</p>
                                     </div>
 
-                                    @guest
-
-                                        <h3><a
-                                                href="{{ route('front.course.details', $course->slug) }}">{{ Str::limit($course->title ?? '', 60) }}</a>
-                                        </h3>
-                                    @endguest
-                                    @auth
-                                        <h3><a
-                                                href="{{ route('course.curriculam', $course->id) }}">{{ Str::limit($course->title ?? '', 60) }}</a>
-                                        </h3>
-
-                                    @endauth
-                                    @guest
-
-                                        <p class="description">{!! Str::limit($course->description ?? '', 60) !!}.</p>
-                                    @endguest
+                                    <h3><a
+                                            href="{{ route('front.course.category', $course->category) }}">{{ Str::limit($course->title ?? '', 60) }}</a>
+                                    </h3>
+                                    <p class="description">{{ Str::limit(strip_tags($course->description ?? ''), 152, '...') }}</p>
 
                                 </div>
                             </div>
